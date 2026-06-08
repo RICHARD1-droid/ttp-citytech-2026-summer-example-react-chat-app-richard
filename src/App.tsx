c3c2e2609ab56475f3e0148ca7a85c37361ed659
Richard Gan <richardgan2003@gmail.com>
2026-06-08 13:42:59 -0400

    initial push

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-//import
-import React from 'react';
+import {useState} from 'react';
 
 const Message = ({text}) => {
     return <>
-        <p>
-            {text}
-        </p>
+      <p>
+        {text}
+      </p>
     </>;
 }
 
 const App = () => {
     //
-    //business logic
+    // business logic
     //
+
+    /*
     const messages = [
-        "Chinese: 你好，世界！",
-        "Spanish: ¡Hola, mundo!",
-        "French: Bonjour, le monde!",
+        "Argentina",
+        "Bolivia",
+        "Cambodia",
     ];
+    */
+
+    const [messages, setMessages] = useState([]);
 
     return <>
         <h1>
@@ -25,22 +29,28 @@ const App = () => {
         </h1>
 
         {
-            messages.map((text, index) => (
-                <Message key={index} text={text} />
-            ))
+            messages.map(
+                (text, index) => <Message key={index} text={text} />
+                )
         }
 
-        <form onSubmit = {(event) => {
+        <form onSubmit={(event) => {
+            event.preventDefault();
 
-            const new_message = event.target.incoming_text
-        }
+            // determine what the new message is
+            const new_message = event.target.incoming_text.value;
 
+            // add the new message to the message state
+            setMessages([...messages, new_message]);
 
-
-        <input />
-        <button>
-            Send
+            // note: this is not best practice in react but it works for now
+            document.getElementById('incoming_text').value = '';
+        }}>
+        <input name='incoming_text' id='incoming_text' />
+        <button type='submit'>
+            send
         </button>
+        </form>
     </>;
 }
 
